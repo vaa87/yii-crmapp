2ee4bc23aa2b6659de3bcfe536db4f66d4aa06ca
Andrey V <vinyaykin@gmail.com>
2018-11-20 20:10:36 +0300

    0.5.1 separated from ORM (63-65)

--- crmapp/controllers/CustomerController.php
+++ crmapp/controllers/CustomerController.php
@@ -10,4 +10,33 @@ class CustomerController extends Controller
         $records = $this->findRecordsByQuery();
         return $this->render('index', compact('records'));
     }
+
+    private function store(Customer $customer)
+    {
+        $customer_record = new CustomerRecord();
+        $customer_record->name = $customer->name;
+        $customer_record->birth_date = $customer->birth_date;
+        $customer_record->notes = $customer->notes;
+
+        $customer_record->save();
+
+        foreach ($customer->phones as $phone) {
+            $phone_record = new PhoneRecord();
+            $phone_record->number = $phone->number;
+            $phone_record->customer_id = $customer_record->id;
+            $phone_record->save();
+        }
+    }
+
+    private function makeCustomer(CustomerRecord $customer_record, PhoneRecord $phone_record)
+    {
+        $name = $customer_record->name;
+        $birth_date = new \DateTime($customer_record->birth_date);
+
+        $customer = new Customer($name, $birth_date);
+        $customer->notes = $customer_record->notes;
+        $customer->phones[] = new Phone($phone_record->number);
+
+        return $customer;
+    }
 }
